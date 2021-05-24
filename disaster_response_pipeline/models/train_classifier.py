5a1a729d639e128f5a2fc8b5210bbf6870856899
Wibo Pipping <wibo@wetransfer.com>
2021-05-24 08:26:35 +0200

    Remove unused parameter from main() in train_classifier.py

--- disaster_response_pipeline/models/train_classifier.py
+++ disaster_response_pipeline/models/train_classifier.py
@@ -181,7 +181,7 @@ def save_model(model, model_filepath):
 @click.command()
 @click.argument('database_filepath', type=click.Path(exists=True))
 @click.argument('model_filepath', type=click.Path())
-def main(database_filepath, model_filepath, p):
+def main(database_filepath, model_filepath):
     """
     Please provide the filepath of the disaster messages database as the first argument and
     the filepath of the pickle file to save the model to as the second argument.
